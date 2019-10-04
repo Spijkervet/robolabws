ae7be778d8f52263d2765364fb79bd4213d6573c
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-10-04 16:24:18 +0200

    Added summary.html netdata

--- run_web.sh
+++ run_web.sh
@@ -2,4 +2,7 @@ sh install_netdata.sh
 sudo apt-get install -y nodejs npm
 sudo npm install -g localtunnel
 
+sudo cp summary.html /usr/share/netdata/web/
+sudo chown netdata:netdata /usr/share/netdata/web/summary.html
+
 nohup lt --port 19999 --subdomain robolabws1 &
